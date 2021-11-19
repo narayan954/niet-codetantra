e102dfcf1b970e8c8c4a7b6e794b61e1ce71f6f0
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-19 23:18:41 +0530

    Create evalPostfix.py
    Write a Python Program to evaluate postfix expression

---
+++ Data structure using python/evalPostfix.py
@@ -0,0 +1,29 @@
+op = {"+","-","*","/","(",",",")","^",}
+
+def eval_postf(exp):
+	stack = []
+	for i in exp:
+		if i not in op:
+			stack.append(i)
+		else:
+			a = int(stack.pop())
+			b = int(stack.pop())
+			
+			if i =="+":
+				res = b+a
+			elif i == "-":
+				res = b-a
+			elif i == "*":
+				res = b*a
+			elif i == "%":
+				res = b%a
+			elif i == "/":
+				res = b/a
+			elif i == "**":
+				res = b**a
+			
+			stack.append(res)
+	return "".join(map(str,stack))
+	
+exp = input("Enter Postfix expression")
+print(f"Result of Postfix expression {exp} is", eval_postf(exp))
