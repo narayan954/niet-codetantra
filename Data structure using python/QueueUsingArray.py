814c19c72bc3ee64799f7ea78378887e0a43969e
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-19 23:25:33 +0530

    Create QueueUsingArray.py
    Program to implement Queue Using array

---
+++ Data structure using python/QueueUsingArray.py
@@ -0,0 +1,31 @@
+def takeInput():
+	print("Select the Operation:")
+	print("1.Enqueue 2.Dequeue 3. Display 4. Quit")
+	return int(input())
+
+class Queue:
+	def __init__(self):
+		self.items = []
+	
+	def enqueue(self):
+		data = input("Enter the element:")
+		self.items.append(data)
+	
+	def dequeue(self):		
+		self.items.pop(0)
+		
+	def display(self):
+		for i in range(len(self.items)):
+			print(self.items[i],'')
+
+size = int(input("Enter the size of Queue:"))
+op = takeInput()
+q = Queue()
+while op != 4:
+	if op == 1:
+		q.enqueue()
+	elif op == 2:
+		q.dequeue()
+	elif op == 3:
+		q.display()
+	op = takeInput()
