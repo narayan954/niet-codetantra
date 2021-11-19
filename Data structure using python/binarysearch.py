a9dd6f404e4c62df6e12b470a5f8ef0d04caa178
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-19 23:21:54 +0530

    Create binarysearch.py
    Program to implement binary search using recursion

---
+++ Data structure using python/binarysearch.py
@@ -0,0 +1,16 @@
+def binary_search(ls,l,r,q):
+	if r < l:
+		return -1
+	mid  = l + (r-l)//2
+	if ls[mid] > q:
+		return binary_search(ls, l, mid-1, q)
+	elif ls[mid] < q:
+		return binary_search(ls, mid+1, r, q)
+	return mid
+
+n = int(input('Enter no of elements'))
+print('enter elements')
+ls = [int(input()) for _ in range(n)]
+q = int(input(' Which element you want to search'))
+answer = binary_search(ls, 0, len(ls)-1,q)
+print(f'The element is present at index  {answer}')
