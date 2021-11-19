22b9cd17aee9be06c28878584c26166194871821
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-19 23:16:59 +0530

    Create stackImplement.py
    Write a program to implement stack using the list

---
+++ Data structure using python/stackImplement.py
@@ -0,0 +1,14 @@
+ls=[]
+while True:
+	n = input('Enter element, \'XXX\' to end: ')
+	if n == 'XXX':
+		break 
+	else:
+		ls.append(n)
+print('Initial stack')
+print(ls)
+print('Elements poped from stack:')
+for i in range(3):
+	print(ls.pop())
+print('Stack after elements are poped:')
+print(ls)
