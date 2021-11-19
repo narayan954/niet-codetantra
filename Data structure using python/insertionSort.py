128df4a70c0b06eccffd0398c0bf40e9f9816381
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-19 23:15:31 +0530

    Create insertionSort.py
    To write a python program Insertion sort.

---
+++ Data structure using python/insertionSort.py
@@ -0,0 +1,13 @@
+ls = list(map(int,input('Enter the list of numbers: ').split()))
+print(list(map(str, ls)))
+for i in range(1,len(ls)):
+	if ls[i]<ls[i-1]:
+		temp = ls.pop(i)
+		for j in range(i-1,-2,-1):
+			if j == -1:
+				ls.insert(0, temp)
+				break
+			if ls[j]<temp:
+				ls.insert(j+1, temp)
+				break
+print(list(map(str,ls)))
