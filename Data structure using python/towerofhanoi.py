f565e0df533479cef6aa172a0d5f787c529702cf
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-19 23:24:11 +0530

    Create towerofhanoi.py
    Program to implement Tower of Hanoi.

---
+++ Data structure using python/towerofhanoi.py
@@ -0,0 +1,10 @@
+def tow(n, start, mid, end):
+	if n == 1:
+		print(f"Move disk {n} from source {start} to destination {end}")
+		return
+	tow(n-1,start,end,mid)
+	print(f"Move disk {n} from source {start} to destination {end}")
+	tow(n-1,mid,start,end)
+	
+n = int(input(" enter no of disk"))
+tow(n,"A","C","B")
