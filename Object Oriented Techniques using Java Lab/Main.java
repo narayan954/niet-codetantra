8439456d1a7986b1261d343dc6e7380e63d2cb78
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 19:05:49 +0530

    Create Main.java
    JAVA program to implement Class Mechanism

---
+++ Object Oriented Techniques using Java Lab/Main.java
@@ -0,0 +1,13 @@
+package q116;
+import java.util.Scanner;
+public class Main{
+	static void str(String a){
+		System.out.println("The entered string is: "+a);
+	}
+	public static void main(String[]args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a string: ");
+		String n = sc.nextLine();
+		str(n);
+	}
+}
