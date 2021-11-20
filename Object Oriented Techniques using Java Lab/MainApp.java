e3544256631ba40b0d7a68e2fd98dc2e8cedae46
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 19:15:12 +0530

    Create MainApp.java
    Write a Java program that implements an interface.

---
+++ Object Oriented Techniques using Java Lab/MainApp.java
@@ -0,0 +1,61 @@
+package q11284;
+interface Car {
+	String getName();
+	int getMaxSpeed();
+	
+	default void applyBreak(){
+		System.out.println("Applying break on" + getName());
+	}
+	
+	static Car getFastestCar(Car car1, Car car2){
+		if(car1.getMaxSpeed() < car2.getMaxSpeed()){
+			return car2;
+		}
+		else{
+			return car1;
+		}
+	}
+	
+}
+class BMW implements Car {
+	String carName;
+	int maxSpeed;
+	
+	public BMW(String name, int maxSpeed){
+		this.carName = name;
+		this.maxSpeed = maxSpeed;
+	}
+	
+	public String getName(){
+		return this.carName;
+	}
+	
+	public int getMaxSpeed(){
+		return this.maxSpeed;
+	}
+	
+}
+class Audi implements Car {
+	String carName;
+	int maxSpeed;
+	
+	public Audi(String name, int maxSpeed){
+		this.carName = name;
+		this.maxSpeed = maxSpeed;
+	}
+	
+	public String getName(){
+		return this.carName;
+	}
+	
+	public int getMaxSpeed(){
+		return this.maxSpeed;
+	}
+}
+public class MainApp {
+	public static void main(String args[]) {
+		BMW bmw = new BMW(args[0],Integer.parseInt(args[1]));
+		Audi audi = new Audi(args[2], Integer.parseInt(args[3]));
+		System.out.println("Fastest car is : " + Car.getFastestCar(bmw,audi).getName());
+	}
+}
