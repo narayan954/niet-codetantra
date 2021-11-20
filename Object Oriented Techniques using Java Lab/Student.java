25cd1a2723fdbf7ead95357547857256f70f1024
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 19:17:18 +0530

    Create Student.java
    Correct the error

---
+++ Object Oriented Techniques using Java Lab/Student.java
@@ -0,0 +1,22 @@
+package q11161;
+public class Student {
+	private String id;
+	private String name;
+	private int age;
+	private char gender;
+	
+	public Student(String name, String rollNo, int age, char gender) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+}
+
+public class StudentMain {
+	public static void main(String[] args) {
+		Student st = new Student("2", "Lakshmi", 26, 'F');
+		System.out.println("Good Job ! ");
+	}
+	
+}
