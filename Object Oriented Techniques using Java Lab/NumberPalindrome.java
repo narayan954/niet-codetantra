ffc86de4541c04b2f67764680736b31e77176ca1
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 19:04:15 +0530

    Create NumberPalindrome.java
    Java Program to check whether the given number is Palindrome or not

---
+++ Object Oriented Techniques using Java Lab/NumberPalindrome.java
@@ -0,0 +1,38 @@
+package q10894;
+
+public class NumberPalindrome {
+	
+	public void isNumberPalindrome(int number) {
+		int r,sum = 0,temp = number;
+		
+		while(number!=0){
+			r = number%10;
+			sum = (sum*10) + r;
+			number = number/10;
+		}
+		number = temp;
+		if (temp == sum) {
+			System.out.println(number + " is a palindrome");
+		}
+		else{
+			System.out.println(number + " is not a palindrome");
+		}
+	}
+}
+
+public class NumberPalindromeMain{
+	public static void main(String[] args){
+		if (args.length < 1) {
+			System.out.println("Usage: java NumberPalindromeMain <number>");
+			System.exit(-1);
+		}
+		try {
+			int number = Integer.parseInt(args[0]);
+			NumberPalindrome nPalindrome = new NumberPalindrome();
+			nPalindrome.isNumberPalindrome(number);
+		} catch(NumberFormatException nfe) {
+			System.out.println("Usage: java NumberPalindromeMain <number>\n\tage: an integer representing number");
+		}
+		
+	}
+}
