9caeba0987205da0b2173ae384572a1de18c4255
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 19:18:01 +0530

    Create Bike2.java
    Write a JAVA program to implement method overriding.

---
+++ Object Oriented Techniques using Java Lab/Bike2.java
@@ -0,0 +1,20 @@
+class methodOverriding{
+	void run() {
+		System.out.println("Vehicle is running");
+	}
+}
+
+class Bike extends methodOverriding {
+	void run() {
+		System.out.println("Bike is running safely");
+	}
+}
+
+public class Bike2{
+	public static void main(String[] args){
+		methodOverriding ob1 = new methodOverriding();
+		Bike ob2 = new Bike();
+		ob1.run();
+		ob2.run();
+	}
+}
