cc6f55b706467d73ba2943054137c36bc1853861
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 19:11:03 +0530

    Create StaticClassDemo.java
    Write a Java program to illustrate the static class concept

---
+++ Object Oriented Techniques using Java Lab/StaticClassDemo.java
@@ -0,0 +1,22 @@
+package q11293;
+class StaticClassDemo{
+	public static class Static{
+		int a,b;
+		public Static(int a, int b){
+			this.a = a;
+			this.b = b;
+		}
+		public void getValues(){
+			System.out.println("a1 = A [value = " + this.a + "]");
+			System.out.println("a2 = A [value = " + this.b + "]");
+		}
+	}
+	public static void main(String[] args){
+		java.util.Scanner sc = new java.util.Scanner(System.in);
+		System.out.print("Enter a number:");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		Static s = new Static(a,b);
+		s.getValues();
+	}
+}
