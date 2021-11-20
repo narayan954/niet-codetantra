ea7e2945a7ab064c5b05a4bfaeac1923e2a139b0
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 19:02:19 +0530

    Create BalanceCheck.java
    Understanding if-then-else statement

---
+++ Object Oriented Techniques using Java Lab/BalanceCheck.java
@@ -0,0 +1,24 @@
+package q10850;
+
+public class BalanceCheck{
+	
+	public static void checkBalance(double balance) {
+		if (balance < 1000)
+		{
+			System.out.println("Balance is low");
+		}
+		else {
+			System.out.println("Sufficient balance");
+		}
+	}
+}
+
+
+public class BalanceMain{
+	public static void main(String[] args){
+		double balance = Double.parseDouble(args[0]);
+		BalanceCheck  bc= new BalanceCheck();
+		bc.checkBalance(balance);
+	}
+	
+}
