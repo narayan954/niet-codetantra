a668753389d631540e6e7283162f41e5d35bc693
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 18:58:35 +0530

    Create PrimitiveTypes.java
     Write a Java program to Display Default values of all Primitive data types

---
+++ Object Oriented Techniques using Java Lab/PrimitiveTypes.java
@@ -0,0 +1,19 @@
+package q10815;
+public class PrimitiveTypes {
+	static byte val1;
+	static short val2;
+	static int val3;
+	static long val4;
+	static boolean val5;
+	static double val6;
+	static float val7;
+	public static void main(String[] args) {
+		System.out.println("byte default value = " + val1);
+		System.out.println("short default value = " + val2);
+		System.out.println("int default value = " + val3);
+		System.out.println("long default value = " + val4);
+		System.out.println("boolean default value = " + val5);
+		System.out.println("double default value = " + val6);
+		System.out.println("float default value = " + val7);
+	}
+}
