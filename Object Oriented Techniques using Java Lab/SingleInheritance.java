e2c2fb51184d80f0212f99909c3a837cf8a50f7a
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 19:13:15 +0530

    Create SingleInheritance.java
    Write a JAVA program to implement Single Inheritance.

---
+++ Object Oriented Techniques using Java Lab/SingleInheritance.java
@@ -0,0 +1,17 @@
+class Example{
+	void show(String a, String b){
+	java.util.Scanner sc = new java.util.Scanner(System.in);
+	System.out.print("Enter the " + a + " class string: ");
+	String c = sc.nextLine();
+	System.out.println(b + " class string is: "+c);
+	}
+}
+
+public class SingleInheritance{
+	public static void main(String[] args){
+	Example ob1 = new Example();
+	ob1.show("first","First");
+	Example ob2 = new Example();
+	ob2.show("second","Second");
+	}
+}
