dd31740a037fc15ab28d997c21523e8699f3a32e
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 19:23:24 +0530

    Create CountOfTwoNumbers.java
    Write a class CountOfTwoNumbers with a public method compareCountOf that takes three parameters one is arr of type int[] and other two are arg1 and arg2 are of type int and returns true if count of arg1 is greater than arg2 in arr. The return type of compareCountOf should be boolean.

---
+++ Object Oriented Techniques using Java Lab/CountOfTwoNumbers.java
@@ -0,0 +1,46 @@
+package q11075;
+import java.util.*; 
+public class CountOfTwoNumbers { 
+  public static void main(String[] args){ 
+      Scanner sc =  new Scanner(System.in); 
+      System.out.println("Enter no of elements in the array:"); 
+      int n = sc.nextInt(); 
+      int arr[] = new int[n]; 
+      System.out.println("Enter the arg1 element:"); 
+      int arg1 = sc.nextInt(); 
+      System.out.println("Enter the arg2 element:"); 
+      int arg2 = sc.nextInt(); 
+      boolean result = compareCountOf(arr, arg1, arg2); 
+      System.out.println(result); 
+      }
+  public static boolean compareCountOf(int[] arr, int arg1, int arg2) { 
+      int c1 = 0, c2 = 0; 
+      for(int i=0; i<arr.length; i++){ 
+        if(arg1 == arr[i])  c1 += 1; 
+        if(arg2 == arr[i])  c2 += 1; 
+      } 
+      if(c1 > c2) return true; 
+      else  return false; 
+    } 
+}
+
+public class CountOfTwoNumbersMain{
+		public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter no of elements in the array:");
+		int n = s.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter elements in the array seperated by space:");
+		for(int i = 0; i < n; i++)
+        {
+		arr[i] = s.nextInt();
+        }
+		System.out.println("Enter the arg1 element:");
+		int arg1 = s.nextInt();
+		System.out.println("Enter the arg2 element:");
+		int arg2 = s.nextInt();
+		CountOfTwoNumbers cNum = new CountOfTwoNumbers();
+		boolean result = cNum.compareCountOf(arr, arg1, arg2);
+		System.out.println(result);
+		}
+}
