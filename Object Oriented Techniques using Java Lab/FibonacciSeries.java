9442dd1d401de3fe921b41aa33a74a7ed6842bb8
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-20 19:05:02 +0530

    Create FibonacciSeries.java
    Java Program to Display the Fibonacci Series

---
+++ Object Oriented Techniques using Java Lab/FibonacciSeries.java
@@ -0,0 +1,16 @@
+package q10896;
+
+public class FibonacciSeries {
+	public static void main(String[] args) {
+		int a=0,b=1,n;
+		n = Integer.parseInt(args[0]);
+		System.out.print(a);
+		System.out.print(" " + b);
+		while(b + a < n){
+			int temp = b;
+			b = b + a;
+			a = temp;
+			System.out.print(" " + b);
+		}
+	}
+}
