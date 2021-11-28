91c3e78d0c455c148c36539c3bfc9d4a31beef91
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-28 16:02:31 +0530

    Create ThrowExample.java
     Write a Java program for creation of illustrating throw

---
+++ Object Oriented Techniques using Java Lab/ThrowExample.java
@@ -0,0 +1,22 @@
+package q11335;
+public class ThrowExample {
+	public static void main(String args[]) {
+		System.out.println("Welcome to the Registration process!!");
+		try {
+			checkEligibilty(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+			System.out.println("Have a nice day");
+			}
+		catch(Exception e) {
+			System.out.println(e);
+			}
+	}
+	static void checkEligibilty(int age, int weight) {
+		if(age < 12 && weight < 40) {
+			throw new ArithmeticException("Student is not eligible for registration");
+			}
+		else {
+			System.out.println("Student Entry is Valid!!");
+			}
+		}	
+	}
+	
