0058cd9795e3cd9f10ddf95857aaaef9ec116a0e
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-28 15:59:54 +0530

    Create Example1.java
    Write a program to implement User-Defined Exception in Java.

---
+++ Object Oriented Techniques using Java Lab/Example1.java
@@ -0,0 +1,20 @@
+class MyException extends Exception {
+	String str1;    MyException(String str2) {
+		str1=str2;
+	} 
+	public String toString(){ 
+		return ("MyException Occurred: "+str1) ; 
+	}    
+}
+class Example1{
+	public static void main(String args[]){
+		try{
+			System.out.println("Starting of try block");
+			throw new MyException("This is My error Message");
+		}
+		catch(MyException exp){
+			System.out.println("Catch Block") ;
+			System.out.println(exp) ;
+		}     
+	}              
+}
