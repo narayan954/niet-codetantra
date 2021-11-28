5a161346c821f395b430860b45d22df4a51f9842
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-28 15:58:17 +0530

    Create BubbleSortingMain.java
    Write a Java program to Sort elements using Bubble Sort

---
+++ Object Oriented Techniques using Java Lab/BubbleSortingMain.java
@@ -0,0 +1,29 @@
+package q11039;
+public class BubbleSorting {
+	public void bubbleSort(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			for (int j = i; j < array.length; j++) {  
+				if (array[i] > array[j]) {   
+					int temp = array[i];  
+					array[i] = array[j];    
+					array[j] = temp;
+				}   
+			}
+		}
+		for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]); 
+		}      
+	}           
+}
+
+public class BubbleSortingMain{
+		public static void main(String[] args) {
+			int[] array = new int[args.length];
+			for (int i = 0; i < args.length; i++)
+			{
+		     array[i] = Integer.parseInt(args[i]);
+			}
+			BubbleSorting bSorting = new BubbleSorting();
+			bSorting.bubbleSort(array);
+	}
+}
