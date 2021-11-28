c91057d24e9fc8ae30409d7fdd042199845c50c9
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-28 16:03:10 +0530

    Create AssertionExample.java
    Write a program to implement the concept of Assertions in JAVA programming language.

---
+++ Object Oriented Techniques using Java Lab/AssertionExample.java
@@ -0,0 +1,13 @@
+//program to implement the concept of Assertions in JAVA programming language.
+//use command prompt for enabling assertions and compilation can be done using cmd 
+package q122;
+import java.util.Scanner;
+class AssertionExample {
+	public static void main( String args[] ) {
+		Scanner scanner = new Scanner( System.in );
+		System.out.print("Enter your age: ");
+		int value = scanner.nextInt();
+		assert value>=18:" Not valid";
+		System.out.println("value is "+value);
+	} 
+}
