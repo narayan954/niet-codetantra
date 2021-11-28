116bd0de079eec632ee7ab8cac326de1ba3c17fd
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-11-28 16:05:18 +0530

    Create Example.java
    The program has a class Example with the main method. The program takes input from the command line argument. Print the output by appending all the capital letters in the input.

---
+++ Object Oriented Techniques using Java Lab/Example.java
@@ -0,0 +1,14 @@
+package q24212;
+
+public class Example{
+	public static void main(String[] args){
+		String str = args[0];
+		String res = "";
+		for (int i=0;i<str.length();i++){
+			if (Character.isUpperCase(str.charAt(i))){
+				res = res + str.charAt(i);
+			}
+		}
+		System.out.println("The result is: "+res);
+	}
+}
