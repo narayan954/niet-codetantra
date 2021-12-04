c16f41112723d95c44ec35c1b353d3e21ca4e1df
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-12-04 16:12:04 +0530

    Create singlelinkedlist.py
    Program to implement the insertion at the front end of the Single Linked list

---
+++ Data structure using python/singlelinkedlist.py
@@ -0,0 +1,39 @@
+def get_input():
+	return int(input("Select a Operation: 1.Insertion 2.Display 3.Quit "))
+
+class Node:
+	def __init__(self, data=None, next=None):
+		self.data = data   
+		self.next = next
+
+class LinkedList: 
+	def __init__(self):    
+		self.head = None 
+		
+	def print_ll(self): 
+		print('The Inserted elements at the front end are :')
+		if self.head is None:  
+			print("Linked list is empty")
+			return     
+		itr = self.head   
+		while itr:    
+			print(itr.data)
+			itr = itr.next
+
+	def insert_at_begining(self):
+		data = int(input('Enter element '))
+		node = Node(data, self.head) 
+		self.head = node  
+
+	
+ll = LinkedList()
+while True:
+	query = get_input()
+	if query == 1:
+		ll.insert_at_begining()
+	elif query == 2:
+		ll.print_ll()
+	elif query == 3:
+		break
+	else:
+		print("Invalid Option!!!")
