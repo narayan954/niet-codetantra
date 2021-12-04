2af63c1060ac103522bf5a6b88e5cdefcbf33b90
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-12-04 16:21:28 +0530

    Create searchingdoublelinkedlist.py
    Program to implement searching in Double Linked List

---
+++ Data structure using python/searchingdoublelinkedlist.py
@@ -0,0 +1,15 @@
+def get_input():
+	return int(input("Select a operation:\n1.Insertion\n2.Searching\n3.Display\n4.Quit	"))	
+ls = []
+while True:	
+	query = get_input()	
+	if query == 1:	
+		ls.append(int(input("Enter Element ")))	
+	elif query == 2:
+		key = int(input("Enter Element to Search "))
+		print(f'Node is present in the list at the position : {ls.index(key)+1}' if key in ls else 'Node is not present in the list')
+	elif query == 3:	
+		for i in ls:			
+			print(i)
+	else:
+		break
