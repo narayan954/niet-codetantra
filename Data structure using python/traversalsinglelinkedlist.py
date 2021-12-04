6b3e3e7b1c6abf638a8d92972fc0119ea7d4340d
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-12-04 16:05:33 +0530

    Create traversalsinglelinkedlist.py
    Program to implement traversal in Single Linked List

---
+++ Data structure using python/traversalsinglelinkedlist.py
@@ -0,0 +1,7 @@
+ls = []
+for _ in range(int(input("Enter how many elements would you like to add: "))):
+	ls.append(int(input("Enter data elements: ")))
+print("The linked list is: ")
+ls = ls[::-1]
+for i in ls:
+	print(i)
