851b91a2c6d637b64389d92da903ca451536907c
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-12-04 16:10:13 +0530

    Create mergesinglelinkedlist.py
    Python program to merge two sorted linked lists

---
+++ Data structure using python/mergesinglelinkedlist.py
@@ -0,0 +1,13 @@
+ls1 = []
+ls2 = []
+
+for _ in range(int(input("Enter number of elements in first list "))):
+	ls1.append(int(input("enter element ")))
+
+for _ in range(int(input("Enter number of elements in second list "))):
+	ls2.append(int(input("enter element ")))
+ls1.extend(ls2)
+ls1.sort()
+print("Merged Linked List is:")
+for i in ls1:
+	print(i, end =' ')
