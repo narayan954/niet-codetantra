15c386c478c202ac2d7f3c7ed915da11ed883ca0
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-12-04 16:09:32 +0530

    Create sortsinglelinkedlist.py
    Program to implement Sorting in Single linked list

---
+++ Data structure using python/sortsinglelinkedlist.py
@@ -0,0 +1,17 @@
+def get_input():
+	return int(input("Select a Operation 1.Insertion 2.Sorting 3.Display 4.Quit "))
+ls = []
+while True:
+	query = get_input()	
+	if query == 1:	
+		ls.append(int(input("Enter a element ")))
+	elif query == 2:
+		if not ls:
+			print('List is Empty')
+		ls.sort()
+		#ls = sorted(ls,reverse=True)
+	elif query == 3:	
+		for i in ls:		
+			print(i)	
+	else:	
+		break
