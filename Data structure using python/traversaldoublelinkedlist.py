55bf0b3b23d9c3580c6d016613fe2e51bca4d2c6
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-12-04 16:20:52 +0530

    Create traversaldoublelinkedlist.py
     Program to implement double linked list traversal in forward direction and traversal in the reverse direction in double linked list

---
+++ Data structure using python/traversaldoublelinkedlist.py
@@ -0,0 +1,9 @@
+ls = []
+for _ in range(int(input("Enter Number of Elements to Insert in DoublyLinkedList "))):
+	ls.append(int(input("Enter Element ")))
+print("Traversal in forward direction")
+for i in ls[::-1]:
+	print(i)
+print("Traversal in reverse direction")
+for i in ls:
+	print(i)
