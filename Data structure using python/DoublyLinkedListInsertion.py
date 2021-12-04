1d4d9b8255792df228bac0327a511d8e95b5b9ae
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-12-04 16:10:54 +0530

    Create DoublyLinkedListInsertion.py
    Program to implement Insertion operation by adding the elements at the end f the doubly Linked List

---
+++ Data structure using python/DoublyLinkedListInsertion.py
@@ -0,0 +1,13 @@
+def get_input():
+	return int(input("Select Opertion\n1.Insertion\n2.Display\n3.Quit	"))
+ls = []
+while True:
+	query = get_input()
+	if query == 1:	
+		ls.append(int(input("enter element ")))
+	elif query == 2:
+		print("Adding a node to the end of the list: ")
+		for i in ls:	
+			print(i)	
+	else:		
+		break
