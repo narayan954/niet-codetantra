99a5b3092e917e534630f7e9cf277568df2fe2f7
Narayan soni <77617189+narayan954@users.noreply.github.com>
2021-12-04 16:07:35 +0530

    Create reversallinkedlist.py
    Program to implement Reversal of a single linked list

---
+++ Data structure using python/reversallinkedlist.py
@@ -0,0 +1,15 @@
+def get_input():
+	return int(input("Select a option: 1.Insertion 2.Reversal 3.Display 4.Quit "))
+
+ls = []
+while True:
+	query = get_input()
+	if query == 1:
+		ls.append(int(input("Enter number ")))
+	elif query == 2:
+		ls = ls[::-1]
+	elif query == 3:
+		for i in ls[::-1]:
+			print(i)
+	else:
+		break
