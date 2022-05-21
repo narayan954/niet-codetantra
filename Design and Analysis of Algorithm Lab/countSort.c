c2f8f43816a301dcbd5aeb7da7a3466d283de77f
Narayan soni <77617189+narayan954@users.noreply.github.com>
2022-05-22 00:03:30 +0530

    Create countSort.c
    Program to sort a list of elements using Counting sort.

---
+++ Design and Analysis of Algorithm Lab/countSort.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int arr[100],s[1000],d[100];
+int i,n,temp;
+printf("enter the no. of arry element: ");
+scanf("%d",&n);
+printf("enter the element: ");
+for(i=0;i<n;i++)
+{
+scanf("%d",&arr[i]);
+}
+for(i=0;i<n;i++)
+{
+s[arr[i]]++;
+}
+for(i=1;i<1000;i++)
+{
+s[i]=s[i]+s[i-1];
+}
+for(i=0;i<n;i++)
+{
+temp=s[arr[i]];
+d[temp-1]=arr[i];
+s[arr[i]]--;
+}
+for(i=0;i<n;i++)
+{
+printf("%d ",d[i]);
+}
+printf("\n");
+}
