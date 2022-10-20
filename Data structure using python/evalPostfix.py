77aa58b200b5d3341d81b297cde1b33ee79e1ae6
Devangshu Singh <112312098+Devangshu1@users.noreply.github.com>
2022-10-20 10:20:50 +0530

    Update evalPostfix.py
    The code which was written before was developing some sort of wrong answers for the question. Hence I got some idea and making changes for the code.

--- Data structure using python/evalPostfix.py
+++ Data structure using python/evalPostfix.py
@@ -1,31 +1,30 @@
-op = {"+", "-", "*", "/", "(", ",", ")", "^", }
-
-
-def eval_postf(exp):
-    stack = []
-    for i in exp:
-        if i not in op:
-            stack.append(i)
-        else:
-            a = int(stack.pop())
-            b = int(stack.pop())
-
-            if i == "+":
-                res = b+a
-            elif i == "-":
-                res = b-a
-            elif i == "*":
-                res = b*a
-            elif i == "%":
-                res = b % a
-            elif i == "/":
-                res = b/a
-            elif i == "**":
-                res = b**a
-
-            stack.append(res)
-    return "".join(map(str, stack))
-
-
-exp = input("Enter Postfix expression")
-print(f"Result of Postfix expression {exp} is", eval_postf(exp))
+class Evaluate:
+	def __init__(self,capacity):
+		self.top=-1
+		self.capacity=capacity
+		self.array=[]
+	def isEmpty(self):
+		return True if self.top==-1 else False
+	def peek(self):
+		return self.array[-1]
+	def pop(self):
+		if not self.isEmpty():
+			self.top-=1
+			return self.array.pop()
+		else:
+			return "$"
+	def push(self,op):
+		self.top+=1
+		self.array.append(op)
+	def evaluate(self,exp):
+		for i in exp:
+			if i.isdigit():
+				self.push(i)
+			else:
+				val1=self.pop()
+				val2=self.pop()
+				self.push(str(eval(val2+i+val1)))
+		return float(self.pop())
+exp=input("Enter Postfix expression")
+obj=Evaluate(len(exp))
+print("Result of Postfix expression",exp,"is",int((obj.evaluate(exp))))
